457afc29f04f6841dea82f48b8df850b550b4c75
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-03-17 14:34:38 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+ 
+ const chai = "chai"
+//const spies = "ch) {
+    
+    
+function Spy(){
+    return 'bleh'
+};
+function receivesAFunction(callback){
+return callback()}
+
+//receivesAFunction(Spy)
+    
+
+
+function returnsANamedFunction(){
+    return receivesAFunction
+
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){}
+}
